7e9f6b205a8a10ceac81439b5ad0baf22987a820
dsabelli <dsabelli3@gmail.com>
2024-08-03 15:19:17 -0400

    add emailJS

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -14,6 +14,16 @@
     const template = document.createElement("template");
     template.innerHTML = htmlContent;
 
+    // Fetch API key from Netlify function
+    // const apiKeyResponse = await fetch("../../netlify/functions/getApiKey.js");
+    // const apiKeyData = await apiKeyResponse.json();
+    // const apiKey = apiKeyData.apiKey;
+    (function () {
+      emailjs.init({
+        publicKey: "jzW08tXATahZEmECf",
+      });
+    })();
+
     // Define the custom element
     class FormElement extends HTMLElement {
       constructor() {
@@ -21,6 +31,21 @@
         const shadowRoot = this.attachShadow({ mode: "open" });
         shadowRoot.adoptedStyleSheets = [styleSheet];
         shadowRoot.appendChild(template.content.cloneNode(true));
+
+        shadowRoot
+          .querySelector("#contact-form")
+          .addEventListener("submit", function (event) {
+            event.preventDefault();
+            // these IDs from the previous steps
+            emailjs.sendForm("service_mmlaf1r", "template_79xcu8k", this).then(
+              () => {
+                window.alert("SUCCESS!");
+              },
+              (error) => {
+                window.alert("FAILED...", error);
+              }
+            );
+          });
       }
     }
     // Define the custom element
