e8f08696b8a0ce2d06afa4cef6bf59306f3fe53b
dsabelli <dsabelli3@gmail.com>
2024-08-10 20:04:18 -0400

    update theme colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    mytheme: {
-      primary: "#e8c988",
-
-      secondary: "#e8e6e3",
-
-      accent: "#5e92ed",
-
-      neutral: "#312735",
-
-      "base-100": "#1e2533",
-
-      info: "#7EBCDD",
-
-      success: "#53EAD6",
-
-      warning: "#F48B0B",
-
-      error: "#F35356",
-    },
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#fed719",
+          secondary: "#e8e6e3",
+          accent: "#5e92ed",
+          neutral: "#626262",
+          "base-100": "#f7f7f7",
+          "base-200": "#eaeaea",
+          "base-300": "#ddd",
+          info: "#7EBCDD",
+          success: "#53EAD6",
+          warning: "#F48B0B",
+          error: "#F35356",
+        },
+      },
+      {
+        myDarkTheme: {
+          primary: "#fed719",
+          secondary: "#e8e6e3",
+          accent: "#5e92ed",
+          neutral: "#626262",
+          "base-100": "#000",
+          "base-200": "#eaeaea",
+          "base-300": "#ddd",
+          info: "#7EBCDD",
+          success: "#53EAD6",
+          warning: "#F48B0B",
+          error: "#F35356",
+        },
+      },
+    ],
   },
+  darkMode: ["class", "[dark-theme='myDarkTheme'"],
   plugins: [require("daisyui")],
 };
