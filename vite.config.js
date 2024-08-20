09769c97ecbbc2fff516fa99b5c947a3981b5562
dsabelli <dsabelli3@gmail.com>
2024-08-20 16:16:58 -0400

    add vite and reconfig for build

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        services: resolve(__dirname, "services.html"),
+        colours: resolve(__dirname, "colours.html"),
+        about: resolve(__dirname, "about.html"),
+        contact: resolve(__dirname, "contact.html"),
+        "cross-ref": resolve(__dirname, "cross-ref.html"),
+        404: resolve(__dirname, "404.html"),
+      },
+    },
+  },
+});
